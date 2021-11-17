772ce35c16bffa58219c7eb41d6febbf6d199636
Edkofifi <atk.eddy@gmail.com>
2021-11-17 23:56:11 +0000

    Products added

--- services/productsService.js
+++ services/productsService.js
@@ -36,13 +36,58 @@ const PRODUCTS = [
         id: 4,
         name: 'Mac Book',
         price: 6000,
-        image: require('../assets/products/mac 2.jpg'),
+        image: require('../assets/products/mac 4.jpg'),
         description: 'To be added soon'
         //color:
         //size: 
         // reviews
         //promo
     },
+    {
+        id: 5,
+        name: 'Mac Book',
+        price: 5100,
+        image: require('../assets/products/mac 5.jpg'),
+        description: 'To be added soon'
+        //color:
+        //size: 
+        // reviews
+        //promo
+    },
+    {
+        id: 6,
+        name: 'Mac Book',
+        price: 4700,
+        image: require('../assets/products/mac 6.jpg'),
+        description: 'To be added soon'
+        //color:
+        //size: 
+        // reviews
+        //promo
+    },
+    {
+        id: 7,
+        name: 'Mac Book',
+        price: 4000,
+        image: require('../assets/products/mac 7.jpg'),
+        description: 'To be added soon'
+        //color:
+        //size: 
+        // reviews
+        //promo
+    },
+    {
+        id: 8,
+        name: 'Mac Book',
+        price: 6500,
+        image: require('../assets/products/mac 8.jpg'),
+        description: 'To be added soon'
+        //color:
+        //size: 
+        // reviews
+        //promo
+    },
+   
    
   
 ];
